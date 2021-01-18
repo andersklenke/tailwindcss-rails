0e3fa4535537e654af6bbce95e5882946a7bbeb1
Anders Klenke <anders_klenke@icloud.com>
2021-01-18 18:00:45 +0100

    Add Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        beige: "#F4EFEA",
+        "vinby-red": "#CC313D",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
